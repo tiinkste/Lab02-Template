ddc0778859d791785c62501132465affda4e0b8d
Art Inkster <tiinkste@cs.ualberta.ca>
2025-09-20 23:36:32 -0600

    Updated check.sh

--- check.sh
+++ check.sh
@@ -4,3 +4,23 @@ set -e
 # DO NOT MODIFY ANY OF THE LINES ABOVE
 
 # TODO: Fill in this file with commands to compile, execute, and check output
+
+gcc -Wall -Werror -std=c99 ./src/ex2q1.c -o ex2q1.out
+
+./src/ex2q1.c < Testcases/1-1-input.txt >| Testcases/1-1-student.txt
+
+diff -u Testcases/1-1-output.txt Testcases/1-1-student.txt
+
+./src/ex2q1.c < Testcases/1-2-input.txt >| Testcases/1-2-student.txt
+
+diff -u Testcases/1-2-output.txt Testcases/1-2-student.txt
+
+./src/ex2q1.c < Testcases/1-3-input.txt >| Testcases/1-3-student.txt
+
+diff -u Testcases/1-3-output.txt Testcases/1-3-student.txt
+
+./src/ex2q1.c < Testcases/1-4-input.txt >| Testcases/1-4-student.txt
+
+diff -u Testcases/1-4-output.txt Testcases/1-4-student.txt
+
+./validate.sh
