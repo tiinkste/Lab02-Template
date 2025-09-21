4d13d8fc2015429e69020e6967b756369e786772
Art Inkster <tiinkste@cs.ualberta.ca>
2025-09-21 00:04:33 -0600

    Added ex2q2 to src

---
+++ src/ex2q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int prompt_coordinates(float *x_p, float *y_p){
+	
+	// Ask what the coordinates of the first point are
+	printf("Point1: ");
+	
+	// If user inputs bad value, scanf fails - return 1
+	if (scanf("%f", x_p) != 1){
+		return 1; } 
+	
+	
+	
+	// All inputs valid - return 0
+	return 0;
+}
+
+
+
+// Main function
+int main(int argc, char* argv[]){
+	
+	
+	
+	
+	
+	
+	
+	
+	sqrtf()
+	
+	return 0;
+	
+}
